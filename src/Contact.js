4a3beeb4d255825fc85732db5ced7871661c65d1
estefaniaderosa <estefania.derosa@gmail.com>
2021-05-04 22:00:51 +0200

    cv english upload

--- src/Contact.js
+++ src/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Contact.css';
 import { useSelector } from "react-redux";
 import Pdf from './PDF/CVESTEFANIADEROSA.pdf' ;
+import PdfEn from './PDFEN/CVESTEFANIADEROSAEN.pdf';
 
 
 function Contact() {
@@ -14,7 +15,7 @@ function Contact() {
         <div className="contact-container">
             <div className="contact"><a href='https://www.linkedin.com/in/estefaniaderosagil/' target='_blank'><img src={`${process.env.PUBLIC_URL}/images/linkedin.png`} alt="LinkedIn icon" className="icon"/><p>LinkedIn</p></a></div>
             <div className="contact" ><a href={'mailto:estefania.derosa@gmail.com'}><img src={`${process.env.PUBLIC_URL}/images/mail.png`} alt="mail icon" className="icon"/><p>Mail</p></a></div>
-            <div className="contact"><a href={Pdf} target="_blank"><img src={`${process.env.PUBLIC_URL}/images/cv.png`} alt="resume icon" className="icon"/><p>CV</p></a></div>
+            <div className="contact"><a href={language === "spanish" ? Pdf : PdfEn } target="_blank"><img src={`${process.env.PUBLIC_URL}/images/cv.png`} alt="resume icon" className="icon"/><p>CV</p></a></div>
             <div className="contact"><a href='https://github.com/estefaniaderosa' target='_blank'><img src={`${process.env.PUBLIC_URL}/images/github.png`} alt="github icon" className="icon"/><p>Github</p></a></div>
             <div className="contact"><a href='https://codepen.io/estefaniaderosa' target='_blank'><img src={`${process.env.PUBLIC_URL}/images/codepen.png`} alt="codepen icon" className="icon"/><p>Codepen</p></a></div>
 
