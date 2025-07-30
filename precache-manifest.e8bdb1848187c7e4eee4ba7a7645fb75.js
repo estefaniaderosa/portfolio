d63980ad831b01beb9d3f70b167bbdb1f3e25fcc
estefaniaderosa <estefania.derosa@gmail.com>
2025-07-30 18:10:46 +0200

    Updates

---
+++ precache-manifest.e8bdb1848187c7e4eee4ba7a7645fb75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d102fd4b8481ce633512f33a566b9afc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a6351b818df843571786",
+    "url": "/portfolio/static/css/main.68adfc77.chunk.css"
+  },
+  {
+    "revision": "6585c78b91e4e70286c7",
+    "url": "/portfolio/static/js/2.8b0ab63b.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/portfolio/static/js/2.8b0ab63b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6351b818df843571786",
+    "url": "/portfolio/static/js/main.33123723.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "651dd48ba95ab005e65ddff897aa09d7",
+    "url": "/portfolio/static/media/14.651dd48b.svg"
+  },
+  {
+    "revision": "5e9337ce55e47916750c5060bb4377d8",
+    "url": "/portfolio/static/media/EstefaniaDeRosa_CV_en.5e9337ce.pdf"
+  },
+  {
+    "revision": "ebdfbacb37a7f72602b6089a0fca843c",
+    "url": "/portfolio/static/media/EstefaniaDeRosa_CV_es.ebdfbacb.pdf"
+  }
+]);
